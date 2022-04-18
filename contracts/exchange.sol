4e0762e1f4e5b273c30a6311b16c00dcc336bef5
Yuval Ardenbaum <ardyuv001@myuct.ac.za>
2022-04-18 23:37:05 +0200

    Fixed test for addLiquidity and added constant product function

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -21,9 +21,10 @@ contract Exchange {
     /// @dev                    Requires that the address of the token is not the 0 address
     /// @dev                    Sets state variable tokenAddress to argument _tokenAddress
     /// @param _tokenAddress    Address of token contract
-    constructor(address _tokenAddress) {
+    constructor(address _tokenAddress) public {
         require(_tokenAddress != address(0));
         tokenAddress = _tokenAddress;
+        token = Token(tokenAddress);
     }
 
     /// FUNCTIONS ///
@@ -33,7 +34,6 @@ contract Exchange {
     ///                         Approve the exchange spending their tokens
     /// @param _tokenAmount     Amount of tokens to add to liquidity pool
     function addLiquidity(uint256 _tokenAmount) public payable {
-        token = Token(tokenAddress);
         token.transferFrom(msg.sender, address(this), _tokenAmount);
     }
 
@@ -43,4 +43,17 @@ contract Exchange {
         return Token(tokenAddress).balanceOf(address(this));
     }
 
+    /// @notice                 Constant product price calculator
+    /// @return                 Ether price per token
+    function getPrice(uint256 y, uint256 x) public pure returns (uint256) {
+        require(y > 0 && x > 0, "invalid input/output reserve");
+        return ( y * 1000) / x;
+    }
+
+    /// @notice                Superior constant product price calculator 
+    /// @return                Price of Ether or token 
+    function getAmount(uint256 inputAmount, uint256 y, uint256 x) private pure return (uint256) {
+        require(y > 0 && x > 0, "invalid input/output reserve");
+        return (inputAmount * x) / (y + inputAmount);
+    }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,37 +1,44 @@
 const { ethers, waffle } = require('hardhat');
 const { expect } = require('chai');
 const { web3 } = require('web3');
+const provider = waffle.provider;
 
 describe("Exchange", () => {
 
 	let exchange, Exchange, token, Token, owner, addr1, addr2, decimals, supply;
 
-	beforeEach(async () =>{
+	beforeEach(async () => {
 		[owner, addr1, addr2, _] = await ethers.getSigners();
-		Exchange = await ethers.getContractFactory('Exchange');
 		Token = await ethers.getContractFactory('Token');
 		decimals = 18;
 		supply = 10000000;
 		token = await Token.deploy('TEST Coin', 'TEST', decimals, supply);
+		Exchange = await ethers.getContractFactory('Exchange');
 		exchange = await Exchange.deploy(token.address);
-		//const provider = waffle.provider;
 	});
 
-	/*describe("Deployment", async () => {
-		it("Should be reverted if token address is the 0x address", async () => {
-			expect(await Exchange.deploy(address(0)))
-				.to.be.reverted;
-		});
-	});*/
-
 	describe("addLiquidity", async () => {
+		
 		it("Adds liquidity to the exchange", async () => {
 			await token.approve(exchange.address, 2);
-			await exchange.addLiquidity.call(2, { value: 1 });
-			expect(await exchange.address.balance)
-				.to.equal(0);
-			//expect(await exchange.getReserve())
-				//.to.equal(2);
+			await exchange.addLiquidity(2, { value: {_hex: '0x1', _isBigNumber: true} });
+			expect(await provider.getBalance(exchange.address))
+				.to.equal(1);
+			expect(await exchange.getReserve())
+				.to.equal(2);
+		});
+	});
+
+	describe("Price", async () => {
+		it("Calculates the correct price for an asset", async () => {
+			await token.approve(exchange.address, 2000);
+			await exchange.addLiquidity(2000, { value: 1000 })
+			const tokenReserve = await exchange.getReserve();
+			const etherReserve = await provider.getBalance(exchange.address);
+			expect(await exchange.getPrice(etherReserve, tokenReserve))
+				.to.equal(500);
+			expect(await exchange.getPrice(tokenReserve, etherReserve))
+				.to.equal(2000);
 		});
 	});
 });
