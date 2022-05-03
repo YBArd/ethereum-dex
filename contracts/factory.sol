df11b8b469a0e6e15fd4db4e857fe3d1d4b39cc2
Yuval Ardenbaum <ardyuv001@myuct.ac.za>
2022-05-03 09:57:00 +0200

    Added a getter method to return the address of an exchange for a given token

--- contracts/factory.sol
+++ contracts/factory.sol
@@ -2,16 +2,24 @@
 
 pragma solidity ^0.8.6;
 
-import "./token.sol";
 import "./exchange.sol";
 
-/// @title					A registry for all token-pair exchanges
-/// @author					Yuval B. Ardenbaum
-/// @notice					Based on Uniswap V1 
-/// @dev 					Restricted to eth-token pairs
+/// @title						A registry for all token-pair exchanges
+/// @author						Yuval B. Ardenbaum
+/// @notice						Based on Uniswap V1 
+/// @dev 						Restricted to eth-token pairs
 contract Factory {
+
+	/// MAPPINGS
+
 	mapping(address => address) public tokenToExchange;
 
+	/// FUNCTIONS
+
+	/// @notice					Initializes exchange contract for a token
+	/// @dev 					Requires all liquidity for a token to be concentrated in one exchange
+	/// @param _tokenAddress	Address of token in the exchange
+	/// @return 				Address of created exchange instance
 	function makeExchange(address _tokenAddress) public returns (address) {
 		require(_tokenAddress != address(0), "No token contract at the zero address");
 		require(tokenToExchange[_tokenAddress] == address(0), "this exchange has already been deployed");
@@ -20,5 +28,12 @@ contract Factory {
 		return address(exchange);
 	}
 
-	
+	/// @notice					Returns address of exchange contract for _tokenAddress
+	/// @dev					Can be used to query factory registry via interface in another contract
+	/// @param _tokenAddress	Address of token in exchange
+	/// @return  				Address of exchange for token
+	function getExchange(address _tokenAddress) public view returns (address) {
+		return tokenToExchange[_tokenAddress];
+	}
+
 }
