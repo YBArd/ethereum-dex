e1998a18a12ac1271ee06c847823a812a8164076
Yuval Ardenbaum <ardyuv001@myuct.ac.za>
2022-05-03 10:24:52 +0200

    Added factoryAddress field to store the contract address of the exchange registry

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -14,6 +14,7 @@ contract Exchange is Token {
 
     address public tokenAddress;
     Token public token;
+    address public factoryAddress;
 
     /// CONSTRUCTORS ///
 
@@ -25,6 +26,7 @@ contract Exchange is Token {
         require(_tokenAddress != address(0));
         tokenAddress = _tokenAddress;
         token = Token(tokenAddress);
+        factoryAddress = msg.sender;
     }
 
     /// FUNCTIONS ///
@@ -34,7 +36,7 @@ contract Exchange is Token {
     ///                         Approve the exchange spending their tokens
     /// @param _tokenAmount     Amount of tokens to add to liquidity pool
     function addLiquidity(uint256 _tokenAmount) public payable returns (uint256) {
-        if(getReserve() == 0) {
+        if(getTokenReserves() == 0) {
             token.transferFrom(msg.sender, address(this), _tokenAmount);
             uint256 lpBalance = address(this).balance;
             _mint(msg.sender, lpBalance);
@@ -42,7 +44,7 @@ contract Exchange is Token {
         }
         else {
             uint256 ethLiquidity = address(this).balance - msg.value;
-            uint256 tokenLiquidity = getReserve();
+            uint256 tokenLiquidity = getTokenReserves();
             uint256 tokenDeposit = (msg.value * tokenLiquidity) / ethLiquidity;
             require(_tokenAmount >= tokenDeposit, "Insufficient token deposit to add liquidity");
             token.transferFrom(msg.sender, address(this), tokenDeposit);     
@@ -54,7 +56,7 @@ contract Exchange is Token {
 
     /// @notice                 Returns the balance of tokens in the liquidity pool
     /// @return                 Balance of tokens in liquidity pool
-    function getReserve() public view returns (uint256) {
+    function getTokenReserves() public view returns (uint256) {
         return Token(tokenAddress).balanceOf(address(this));
     }
 
@@ -68,15 +70,18 @@ contract Exchange is Token {
     /// @notice                Output amount for desired token during trades
     /// @return                Price of Ether or token 
     function getAmount(uint256 inputAmount, uint256 y, uint256 x) private pure returns (uint256) {
-        require(y > 0 && x > 0, "invalid input/output reserve");
-        return ((inputAmount * x * 1000000000000000000) / (y + inputAmount));
+        require(y > 0 && x > 0, "insufficient eth/token liquidity");
+        uint256 feeAdjustedInput = inputAmount * 99;
+        uint256 numerator = feeAdjustedInput * x;
+        uint256 denominator = (y * 100) + feeAdjustedInput;
+        return (numerator/denominator);
     }
 
     /// @notice                 Calculates amount of token that can be purchased with eth
     /// @return                 Tokens purchased
     function getTokenAmount(uint256 _ethSold) public view returns (uint256) {
         require(_ethSold > 0, "ethSold is invalid");
-        uint256 tokenReserve = getReserve();
+        uint256 tokenReserve = getTokenReserves();
         return getAmount(_ethSold, address(this).balance, tokenReserve);
     }
 
@@ -84,13 +89,13 @@ contract Exchange is Token {
     /// @return                 Eth purchased
     function getEthAmount(uint256 _tokenSold) public view returns (uint256) {
         require(_tokenSold > 0, "tokenSold is invalid");
-        uint256 tokenReserve = getReserve();
+        uint256 tokenReserve = getTokenReserves();
         return getAmount(_tokenSold, tokenReserve, address(this).balance);
     }
 
     /// @notice                 Swaps ether for tokens
     function swapEthToToken(uint256 _baseTokensRequested) public payable {
-        uint256 reserve = getReserve();
+        uint256 reserve = getTokenReserves();
         uint256 tokenQuote = getAmount(msg.value, address(this).balance - msg.value, reserve);
         require(tokenQuote >= _baseTokensRequested, "Insufficient liquidity to perform this trade");
         token.transfer(msg.sender, tokenQuote);
@@ -98,10 +103,23 @@ contract Exchange is Token {
 
     /// @notice                 Swaps tokens for ether
     function swapTokenToEth(uint256 _tokenSold, uint256 _baseEthRequested) public payable {
-        uint256 reserve = getReserve();
+        uint256 reserve = getTokenReserves();
         uint256 ethQuote = getAmount(_tokenSold, reserve, address(this).balance);
         require(ethQuote >= _baseEthRequested, "Insufficient liquidity");
         token.transferFrom(msg.sender, address(this), _tokenSold);
         payable(msg.sender).transfer(ethQuote);
     }
+
+    /// @notice                 Removes eth/token liquidity from exchange contract proportionally
+    /// @param _amount          Amount of LP tokens representing share of liquidity to remove                  
+    /// @return                 Returns ether amount and token amount removed from LPs
+    function removeLiquidity(uint256 _amount) public returns (uint256, uint256) {
+        require(_amount > 0, "Can't remove zero liquidity");
+        uint256 ethers = (address(this).balance * _amount) / getTotalSupply(); // ratio of ethers to total LP reserves
+        uint256 tokens = (getTokenReserves() * _amount) / getTotalSupply(); // ratio of tokens to total LP reserves
+        _burn(msg.sender, _amount); // burns lp tokens
+        payable(msg.sender).transfer(ethers);
+        token.transfer(msg.sender, tokens);
+        return (ethers, tokens);
+    }                
 }
